ecc2d18849a3549841d980b99db4817f1e258693
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-12 13:42:01 +0530

    Create Add_two_num.cpp

---
+++ Add_two_num.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1, n2, t;
+    cin >> t;
+    while (t != 0)
+    {
+        cin >> n1 >> n2;
+        cout << n1 + n2 << endl;
+
+        t--;
+    }
+}
