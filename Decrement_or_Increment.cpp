c7d9765c10541d88de3f089a40412b9cdb540346
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-01-30 07:16:17 +0530

    Create Decrement_or_Increment.cpp

---
+++ Decrement_or_Increment.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cin >> num;
+    if (num % 4 == 0)
+    {
+        cout << num + 1 << endl;
+    }
+    else
+    {
+        cout << num - 1 << endl;
+    }
+}
