0f2de9f0068ab78e39918cd21ec95e8f0b50c16d
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-04-23 10:01:46 +0530

    Create Raju_and_his_trip.cpp

---
+++ Raju_and_his_trip.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if ((a % 6 == 0 || a % 5 == 0))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+}
